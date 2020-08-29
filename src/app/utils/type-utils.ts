ff3928f24ae69703f5e68a75a6c3e347cbdc54ac
Sam <phreakdawg@gmail.com>
2020-08-29 02:07:40 -0600

    Feature/poc (#2)
    * working tests
    
    * fixes #1

--- src/app/utils/type-utils.ts
+++ src/app/utils/type-utils.ts
@@ -1,8 +1,19 @@
-export function isPojoOf<T>(val: any, ...props: string[]): val is T {
+/**
+ * 
+ * @param val object to be evaluated
+ * @param props list of properties on the interface
+ */
+export function isPojoOf<T extends object>(val: any, ...props: (keyof T)[]): val is T {
     // return null for falsy types
     if (!val) {
         return false;
     }
 
-    return true;
+    if (typeof val !== 'object') {
+        return false;
+    }
+
+    return props.reduce((res: boolean, prop: keyof T) => {
+        return res && prop in val;
+    }, true);
 }
