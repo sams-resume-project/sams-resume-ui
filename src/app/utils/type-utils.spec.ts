18ed9a4792dadd84f78a944a6c2cda59fbd7b950
Sam Haycraft <phreakdawg@gmail.com>
2020-08-29 02:24:32 -0600

    one hundo baby

--- src/app/utils/type-utils.spec.ts
+++ src/app/utils/type-utils.spec.ts
@@ -8,7 +8,7 @@ describe('type utilities [type.utils.ts]', () => {
     it('should return false for a falsy object', () => {
         const input: Foo = null;
 
-        const result: boolean = isPojoOf(input, 'bar');
+        const result: boolean = isPojoOf<Foo>(input, 'bar');
 
         expect(result).toBeFalse();
     });
@@ -28,7 +28,15 @@ describe('type utilities [type.utils.ts]', () => {
             foobar: 42,
         };
 
-        const result: boolean = isPojoOf(input, 'bar');
+        const result: boolean = isPojoOf<Foo>(input, 'bar');
+
+        expect(result).toBeFalse();
+    });
+
+    it('should return false if object is a primitive type', () => {
+        const input: any = 'string';
+
+        const result: boolean = isPojoOf<Foo>(input, 'bar');
 
         expect(result).toBeFalse();
     });
