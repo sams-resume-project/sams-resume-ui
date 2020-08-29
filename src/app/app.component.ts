660db5f123d7e9d0a053eb52d041d9e8a17f5bd7
Sam Haycraft <phreakdawg@gmail.com>
2020-08-28 22:05:21 -0600

    basic skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'interface-poc';
+  public firstName: string = 'James';
+  public lastName: string = 'Bond';
 }
