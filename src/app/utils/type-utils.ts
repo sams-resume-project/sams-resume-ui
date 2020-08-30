afb9abee01a02fb43dae20ed304bf425c2cea79a
Sam <phreakdawg@gmail.com>
2020-08-30 02:30:32 -0600

    adds some documenation for typescript utils documentation fixes #13 (#24)
    Fixes #13

--- src/app/utils/type-utils.ts
+++ src/app/utils/type-utils.ts
@@ -1,7 +1,48 @@
 /**
  *
+ * This method tests  if a given object  confirms to an interface of type T
+ *
+ * @method isPojoOf
+ *
+ * @template T a generic pojo type
  * @param val object to be evaluated
  * @param props list of properties on the interface
+ *
+ * @returns true if val is of type T
+ *
+ * @example <caption>Testing for interface with one property</caption>
+ *
+ * interface Foo {
+ *     bar: number,
+ * }
+ *
+ * const x = {
+ *     bar: 42,
+ * };
+ *
+ * // returns true
+ * isPojoOf<Foo>(x, 'bar');
+ *
+ * // transpilation error
+ * isPojoOf<Foo>(x, 'foobar');
+ *
+ * @example <caption>Testing for interface with multiple properties</caption>
+ *
+ * interface Foo {
+ *     bar: number,
+ *     id: number,
+ * }
+ *
+ * const x = {
+ *     bar: 42,
+ *  }
+ *
+ *  // returns true
+ *  isPojoOf<Foo>(x, 'bar');
+ *
+ *  // returns false
+ *  isPojoOf<Foo>(x, 'bar', 'id');
+ *
  */
 export function isPojoOf<T extends object>(val: any, ...props: (keyof T)[]): val is T {
     // return null for falsy types
